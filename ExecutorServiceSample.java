21ec18d1fe8047998f791dea7a19a3672a7ac98c
Brijesh <212393369@logon.ds.ge.com>
2018-07-15 11:23:07 +0530

    ExecutorService sample

---
+++ ExecutorServiceSample.java
@@ -0,0 +1,32 @@
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class ExecutorServiceSample
+{
+	public static void main(String[] args)
+	{
+		ExecutorService executor = Executors.newFixedThreadPool(10);
+		
+		for(int i=0;i<10;i++)
+		{
+			worker wrk = new worker(i);
+			executor.execute(wrk);
+		}
+		executor.shutdown();
+	}
+}
+
+class worker implements Runnable
+{
+	private int number;
+	
+	public worker(int number)
+	{
+		this.number=number;
+	}
+	
+	public void run() {
+		System.out.println("Running "+number);
+	}
+	
+}
