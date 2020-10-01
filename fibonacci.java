55446b46f690f9ef00ec5b1b2ae921f9abe6a9cc
Nirupama Prasad <66474269+Nirupama654@users.noreply.github.com>
2020-10-01 21:16:43 +0530

    Update fibonacci.java

--- fibonacci.java
+++ fibonacci.java
@@ -1,11 +1,12 @@
+// To print fibonacci series i.e. 0 1 1 2 3  5  8  12  20 ........ upto n terms
 public class fibonacci
 {
 	public static void main(String[] args)
 	{
-		int no = 10;
-		int lastTolastNo=0;
-		int lastNo=0;
-		int currNo=0;
+		int n = 10;
+		int first = 0;
+		int second =0;
+		int third = 0;
 		int[] fibbo = new int[no];
 		
 		for(int i=0;i<=fibbo.length-1;i++)
@@ -13,19 +14,19 @@ public class fibonacci
 			if(i==0)
 			{	
 				fibbo[i]=0;
-				lastTolastNo=0;
+				first=0;
 			}	
 			else if(i==1)
 			{	
 				fibbo[i]=1;
-				lastNo=1;
+				second =1;
 			}
 			else
 			{	
-				currNo=lastTolastNo+lastNo;
-				fibbo[i]=currNo;
-				lastTolastNo=lastNo;
-				lastNo=currNo;
+				third = first + second;
+				fibbo[i]=third;
+				first = second;
+				second = third ;
 			}	
 		}
 		for(int i:fibbo)
